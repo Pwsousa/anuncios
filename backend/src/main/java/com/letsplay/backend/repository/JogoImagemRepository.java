05a2ab1038c576622f13d4fa231f5f6648517fc1
Pedro Wilson <87543126+Pwsousa@users.noreply.github.com>
2023-12-11 19:56:38 -0300

    Revert "Backend crud permissoes"

--- backend/src/main/java/com/letsplay/backend/repository/JogoImagemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.letsplay.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.letsplay.backend.entity.JogoImagem;
-
-public interface JogoImagemRepository extends JpaRepository<JogoImagem,Long>{
-    
-}
