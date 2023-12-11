9f0c7ef4afd9a1fb76a741d10d868677917bcc11
Pedro Wilson <87543126+Pwsousa@users.noreply.github.com>
2023-12-11 17:18:07 -0300

    Revert "Backend crud pessoa"

--- backend/src/main/java/com/letsplay/backend/repository/DistribuidoraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.letsplay.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.letsplay.backend.entity.Distribuidora;
-
-public interface DistribuidoraRepository extends JpaRepository<Distribuidora,Long>{
-    
-}
--- backend/src/main/java/com/letsplay/backend/repository/EstadoRespository.java
+++
@@ -1,9 +0,0 @@
-package com.letsplay.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.letsplay.backend.entity.Estado;
-
-public interface EstadoRespository extends JpaRepository<Estado,Long>{
-
-        
-}
